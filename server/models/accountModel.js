681ab7567848baee98a68fbb1be4a43dd948a40e
GeeksforGeeks <rlmagtira@gmail.com>
2025-03-03 16:32:40 +0800

    Server update
    // additional schema update

--- server/models/accountModel.js
+++ server/models/accountModel.js
@@ -6,14 +6,35 @@ name: {
     type: String,
     required: true, 
 }, 
-
 lastName: {
     type: String,
     required: true,
-    
-} 
+},
+email:{
+    type: String,
+    required: true
+},
+number: {
+    type: Number,
+    required: false
+},
 
-
-
-
+address: {
+street: {
+type: String,
+required: true,
+},
+brgy: {
+type: String,
+required: true,
+},
+city:{
+type: String,
+required: true,
+}, 
+province: {
+type: String,
+required: true,
+},
+}
 })
